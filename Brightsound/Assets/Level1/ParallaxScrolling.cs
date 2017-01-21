53fa2dc92e6a1f6f5c8f00988524dc2de2ee656f
pgtruong <petergtruong@gmail.com>
2017-01-20 22:36:01 -0800

    Parallax Mapping Done
    Can scroll through and reset backgrounds and any foregrounds we need.
    -Dash and Pete

--- Brightsound/Assets/Level1/ParallaxScrolling.cs
+++ Brightsound/Assets/Level1/ParallaxScrolling.cs
@@ -1,16 +1,75 @@
-﻿using System.Collections;
+﻿//https://www.youtube.com/watch?v=QkisHNmcK7Y tutorial
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxScrolling : MonoBehaviour {
+    public bool scrolling, parallax;
+    public float backgroundSize;
+    public float parallaxSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Get camera transform and the layers for bg and fg
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private float viewZone = 10;
+    private int leftIndex;
+    private int rightIndex;
+    private float lastCameraX;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraX = cameraTransform.position.x;
+        layers = new Transform[transform.childCount];
+        for(int i = 0; i < transform.childCount; i++)        
+            layers[i] = transform.GetChild(i);
+
+        leftIndex = 0;
+        rightIndex = layers.Length - 1;        
+    }
+
+    void Update()
+    {
+        if (parallax)
+        {
+            float deltaX = cameraTransform.position.x - lastCameraX;
+            transform.position += Vector3.right * (deltaX * parallaxSpeed);
+        }
+
+
+        lastCameraX = cameraTransform.position.x;
+        if (scrolling)
+        {
+            if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
+                ScrollLeft();
+
+            if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+                ScrollRight();
+        }
+
+    }
+    //Resets position when scrolled too far left and vice versa for right
+    void ScrollLeft()
+    {
+        int lastRight = rightIndex;
+        layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+        leftIndex = rightIndex;
+        rightIndex--;
+        if (rightIndex < 0)
+        {
+            rightIndex = layers.Length - 1;
+        }
+    }
+
+    void ScrollRight()
+    {
+        int lastLeft = leftIndex;
+        layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+        rightIndex = leftIndex;
+        leftIndex++;
+        if (leftIndex == layers.Length)
+        {
+            leftIndex = 0;
+        }
+    }
 }
