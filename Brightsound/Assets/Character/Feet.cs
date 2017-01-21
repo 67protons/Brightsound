0ef3575d56a2a4d4809fe52bafe30fb3f72aaf26
Edwin Ho <eaho555@gmail.com>
2017-01-20 21:22:30 -0800

    Basic player movement added

---
+++ Brightsound/Assets/Character/Feet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feet : MonoBehaviour {
+
+    [HideInInspector]
+    public bool isGrounded = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ Brightsound/Assets/Character/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    public Feet feet;
+
+    public float speed = 5f;
+    public float jumpForce = 500f;
+    public float jumpDuration = 0.5f;
+    private float moveDirection = 0f;
+
+    void Awake()
+    {
+        this.rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && feet.isGrounded)
+        {
+            Jump();
+        }
+
+        //Debug.Log(rigidBody.velocity);
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(moveDirection * speed * Time.deltaTime, 0f, 0f));
+    }
+
+    void Jump()
+    {
+        //Vector2 force = (transform.up * jumpForce) / Time.fixedDeltaTime;
+        StopCoroutine(Accelerate(0f));
+        StartCoroutine(Accelerate(jumpDuration));
+        
+    }
+
+    IEnumerator Accelerate(float lifetime)
+    {
+        rigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        yield return new WaitForSeconds(lifetime);
+        rigidBody.velocity = Vector2.zero;
+    }
+}
