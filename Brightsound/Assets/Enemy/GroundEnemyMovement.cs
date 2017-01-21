c15980b79ac37bda7b37de60e450958dc334c162
pgtruong <petergtruong@gmail.com>
2017-01-20 20:59:42 -0800

    Enemy Ground Movement -Dash and Pete
    Includes movement, colliders, and range of distance to move back and
    forth.

---
+++ Brightsound/Assets/Enemy/GroundEnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemyMovement : MonoBehaviour {
+    //Speed and direction of the enemy
+    public float speed = 10f;
+    Vector2 direction = Vector2.left;
+    //Boundaries for enemy ai to move backwards
+    public float range = 6;
+    float distanceTravelled = 0;
+
+    //Moves enemies towards direction
+	void Update () {
+        transform.Translate(direction * speed * Time.deltaTime);
+        distanceTravelled += Mathf.Abs(direction.x * speed * Time.deltaTime);
+        Debug.Log(distanceTravelled);
+        if (distanceTravelled >= range)
+        {
+            ChangeDirection();           
+        }
+	}
+
+    //Changes directions when colliding with boundaries or anything
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        ChangeDirection();
+    }
+
+    //Function that changes direction of enemy movement
+    void ChangeDirection()
+    {
+        if (direction == Vector2.left)
+            direction = Vector2.right;
+        else
+            direction = Vector2.left;
+        distanceTravelled = 0;
+    }
+}
