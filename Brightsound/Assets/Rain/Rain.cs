152d866d71d6672af93265e3b4213342f70fd9d6
Edwin Ho <eaho555@gmail.com>
2017-01-21 22:04:28 -0800

    Rain added

---
+++ Brightsound/Assets/Rain/Rain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rain : MonoBehaviour {
+
+    Player player;
+    float offsetY;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        offsetY = this.transform.position.y;
+    }
+
+    void LateUpdate()
+    {
+        this.transform.position = new Vector3(player.transform.position.x, offsetY, 0f);
+    }
+}
