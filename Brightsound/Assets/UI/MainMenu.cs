0b78c3dbd77e65743022476f8c3d95497e14c512
Edwin Ho <eaho555@gmail.com>
2017-01-22 13:58:21 -0800

    Started Main Menu

---
+++ Brightsound/Assets/UI/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void ClickStart()
+    {
+        MasterGameManager.instance.sceneManager.LoadScene("Level1", false);
+    }
+
+    public void ClickQuit()
+    {
+        Application.Quit();
+    }
+}
