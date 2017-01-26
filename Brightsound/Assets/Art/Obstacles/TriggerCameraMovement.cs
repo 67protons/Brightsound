b6027f51e97338e3610219bca5c14dd444a41aa6
Dashiell Powers <dashiellp@gmail.com>
2017-01-25 22:05:00 -0800

    Fixed camera trigger script
    All camera triggers are now identical and we can have multiple instances
    in the same scene
    
    I also changed the last puzzle again but it still needs work

--- Brightsound/Assets/Art/Obstacles/TriggerCameraMovement.cs
+++ Brightsound/Assets/Art/Obstacles/TriggerCameraMovement.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class TriggerCameraMovement : MonoBehaviour {
 
     //Camera Stuff
-    public Camera mainCam;
+    Camera mainCam;
     CameraController mainCamScript;
+
     //Player
     public GameObject player;
+
     //Smoothness
     public float dampTime = 400f;
     private Vector3 velocity = Vector3.zero;
+
     //Public Variables to control
-    public bool enter;
+    public bool hasEntered;
     public bool follow;
+    public bool isRefocussing;
     Vector3 minValDef;
+
     //size value on camera
     public float zoom = 4;
     private float defaultZoom = 4;
+
     //Target position
     public Vector3 target;
     Vector3 minCamPos;
@@ -27,67 +33,72 @@ public class TriggerCameraMovement : MonoBehaviour {
     public float orthoSpeed = 6f;
     public float distSpeed = 1f;
 
-    public GameObject exitTrigger;
+    public GameObject pairedTrigger;
 
-    //entrance or exit?
-    public bool entrance;
-    bool exit;
+    //Which side?
+    public bool isLeftTrigger;
+    bool isRightTrigger;
+
     public bool cutscene = false;   
     
     void Start()
-    {        
+    {     
+        mainCam = Camera.main;   
         mainCamScript = mainCam.GetComponent<CameraController>();
         minValDef = mainCamScript.minCameraPos;
+        if (pairedTrigger != null)
+        {
+            isLeftTrigger = this.transform.position.x < pairedTrigger.transform.position.x;
+            isRightTrigger = !isLeftTrigger;
+            //Check if player starts between triggers
+            if (isLeftTrigger)
+            {
+                hasEntered = (player.transform.position.x > transform.position.x) && (player.transform.position.x < pairedTrigger.transform.position.x);
+                pairedTrigger.GetComponent<TriggerCameraMovement>().hasEntered = hasEntered;
+            }
+        } else
+        {
+            hasEntered = false;
+        }
     }
     void Update()
     {
-        //if you entered the entrance
-        if (enter && entrance && !exitTrigger.GetComponent<TriggerCameraMovement>().exit)
+        //Make sure the camera only moves when we want it to
+        if (isRefocussing)
         {
-            mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, zoom, Time.deltaTime * orthoSpeed);
-            //if camera should follow
-            if (follow)
+            if (hasEntered)
             {
-                mainCamScript.minCameraPos = Vector3.Lerp(mainCamScript.minCameraPos, new Vector3(minValDef.x, minValDef.y + (zoom - defaultZoom), minValDef.z), Time.deltaTime);
-                //new Vector3(minValDef.x, minValDef.y + (zoom - defaultZoom), minValDef.z);
-            }
-            else {
-                mainCamScript.target = null;                
-                mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, target, Time.deltaTime * distSpeed);
-            }            
-        }
-        
-        //else if its the exit and you exited
-        else if (!entrance && exit)
-        {
-            mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, defaultZoom, Time.deltaTime * orthoSpeed);
-            if (follow)
-            {
-                mainCamScript.minCameraPos = Vector3.Lerp(mainCamScript.minCameraPos, new Vector3(minValDef.x, minValDef.y, minValDef.z), Time.deltaTime);
-                //new Vector3(minValDef.x, minValDef.y + (zoom - defaultZoom), minValDef.z);
-            }
-            else {
-                mainCamScript.target = player.transform;
-                if (mainCamScript.target = player.transform)
+                mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, zoom, Time.deltaTime * orthoSpeed);
+                //if camera should follow
+                if (follow)
                 {
-                    mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, defaultZoom, Time.deltaTime * orthoSpeed);
+                    mainCamScript.minCameraPos = Vector3.Lerp(mainCamScript.minCameraPos, new Vector3(minValDef.x, minValDef.y + (zoom - defaultZoom), minValDef.z), Time.deltaTime);
+                    if ((mainCamScript.minCameraPos - new Vector3(minValDef.x, minValDef.y + (zoom - defaultZoom), minValDef.z)).magnitude <= 0.1f)
+                        isRefocussing = false;
                 }
-            }
-        }
-        //If you leave the entrance
-        else if (!enter && entrance)
-        {
-            if (follow)
-            {
-                mainCamScript.minCameraPos = Vector3.Lerp(mainCamScript.minCameraPos, minValDef, Time.deltaTime);
-                mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, defaultZoom, Time.deltaTime * orthoSpeed);
+                else
+                {
+                    mainCamScript.target = null;
+                    mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, target, Time.deltaTime * distSpeed);
+                    if ((mainCam.transform.position - target).magnitude <= 0.1f)
+                        isRefocussing = false;
+                }
             }
             else
             {
-                mainCamScript.target = player.transform;
-                if (mainCamScript.target = player.transform)
+                mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, defaultZoom, Time.deltaTime * orthoSpeed);
+                mainCamScript.minCameraPos = Vector3.Lerp(mainCamScript.minCameraPos, new Vector3(minValDef.x, minValDef.y, minValDef.z), Time.deltaTime);
+                if (follow)
                 {
+                    if ((mainCamScript.minCameraPos - new Vector3(minValDef.x, minValDef.y, minValDef.z)).magnitude <= 0.1f)
+                        isRefocussing = false;
+                }
+                else
+                {
+                    mainCamScript.target = player.transform;
                     mainCam.orthographicSize = Mathf.Lerp(mainCam.orthographicSize, defaultZoom, Time.deltaTime * orthoSpeed);
+                    if (mainCam.orthographicSize - defaultZoom <= 0.1f)
+                        isRefocussing = false;
                 }
             }
         }
@@ -98,24 +109,18 @@ public class TriggerCameraMovement : MonoBehaviour {
     {
         if (other.CompareTag("Player"))
         {
-            if (entrance)
+            //Check if we're between the triggers
+            hasEntered = (other.transform.position.x > transform.position.x) == isLeftTrigger;
+            isRefocussing = true;
+            if (pairedTrigger != null)
             {
-                if (other.transform.position.x > transform.position.x)
-                    enter = true;
-                else
-                    enter = false;
-            }
-            else
-            {
-                if (other.transform.position.x > transform.position.x)
-                {
-                   // enter = false;
-                    exit = true;
-                }
-                else {
-                    //enter = true;
-                    exit = false;
-                }
+                //Ensure that the paired trigger agrees with the first trigger we entered
+                pairedTrigger.GetComponent<TriggerCameraMovement>().hasEntered = hasEntered;
+                pairedTrigger.GetComponent<TriggerCameraMovement>().follow = follow;
+                pairedTrigger.GetComponent<TriggerCameraMovement>().target = target;
+                pairedTrigger.GetComponent<TriggerCameraMovement>().zoom = zoom;
+                pairedTrigger.GetComponent<TriggerCameraMovement>().orthoSpeed = orthoSpeed;
+                pairedTrigger.GetComponent<TriggerCameraMovement>().distSpeed = distSpeed;
             }
         }            
     }
